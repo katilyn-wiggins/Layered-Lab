9311527ca30f3a6a71771b577b42a325c3c20c72
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-04-05 21:12:31 -0400

    added very basic front end, submit function working

---
+++ public/index.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('anon');
+const ul = document.getElementById('anon-messages');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const fd = new FormData(form);
+
+    fetch('/api/contact', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+            messageSubject: fd.get('subject'),
+            messageBody: fd.get('message'),
+        }),
+    })
+        .then((res) => res.json())
+        .then((json) => console.log(json));
+});
+
+fetch('/api/contact')
+    .then((res) => res.json())
+    .then((totalMessage) => {
+        totalMessage.forEach((message) => {
+            const li = document.createElement('li');
+            li.textContent = `${message.messageSubject} ${message.messageBody}`
+            ul.appendChild(li);
+        });
+    });
